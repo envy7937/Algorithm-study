cc8f61ab7b8f91150d64c644eb35674746ef449c
김경식 <envy7937@gmail.com>
2023-03-08 16:50:46 +0900

    Feet: programmers 120923, 120924, 120956

--- programmers/120923/solution.js
+++ programmers/120923/solution.js
@@ -1,27 +1,12 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=javascript
+// https://school.programmers.co.kr/learn/courses/30/lessons/120923?language=javascript
 
-/*
- * 1. replace를 이용하여 일치하는 단어를 '' 으로 치환한 경우
- *    wyeoo 같은 단어는 ye가 먼저 제거되어 woo가 되고 이 단어 또한 일치하게된다.
- * 2. 정규식을 이용하여 해당 단어로만 이루어진 문장인지를 우선 체크하는 방법을 사용.
- * 3. 정규식을 쓰지 않을 경우 단어를 '' 으로 치환하는것이 아닌 특수문자 등으로 치환하여 검사하는 것도 가능.
- */
-function solution(babbling) {
-  let answer = 0;
-  let canSpeakWords = ['aya', 'ye', 'woo', 'ma'];
+const solution = (num, total) => {
+  let min = Math.ceil(total / num) - Math.floor(num/2);
+  let answer = []
 
-  babbling.map(item => {
-    // \b(경계 메타 문자)를 이용하여 주어진 단어로만 구성도니 문제인지를 확인
-    let regex = new RegExp(`^\\b(${canSpeakWords.join('|')})+\\b`);
-
-    if (regex.test(item)) {
-      canSpeakWords.map(word => {
-        item = item.replace(word, '');
-      })
-
-      if (item == '') answer += 1
-    }
-  })
+  for (let i=0; i<num; i++) {
+    answer.push(min + i);
+  }
 
   return answer;
 }
--- programmers/120924/solution.js
+++ programmers/120924/solution.js
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=javascript
+// https://school.programmers.co.kr/learn/courses/30/lessons/120924?language=javascript
 
 const solution = c => (c[1] - c[0] === c[2] - c[1])
   ? (c[c.length - 1] + (c[1] - c[0]))
