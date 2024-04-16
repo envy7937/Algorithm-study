8d2b993a6d6a0fc53a91b5ed575c7d550748da09
김경식 <ks.kim@stickint.kr>
2024-04-16 11:03:21 +0900

    :wrench: Modify .gitignore

--- programmers/120912/solution.js
+++
@@ -1,5 +0,0 @@
-https://school.programmers.co.kr/learn/courses/30/lessons/120912
-
-const solution = (array) => array.join('').split(7).length - 1;
-
-// const solution = (array.join('').match(/7/g) ? array.join('').match(/7/g).length : 0);
